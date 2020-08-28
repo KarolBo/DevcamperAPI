e19ae00498d2584b0fc295973eee37cefad50a9d
Karol Borkowski <karol.borkowski@b-rayz.ch>
2020-08-28 21:41:28 +0200

    error handling

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -1,6 +1,7 @@
 const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require('../utils/errorResponse');
 
-exports.getBootcamps = async (req, res) => {
+exports.getBootcamps = async (req, res, next) => {
     try{
         const bootcamps = await Bootcamp.find();
         res.status(200).json({ 
@@ -8,28 +9,24 @@ exports.getBootcamps = async (req, res) => {
             count: bootcamps.length,
             data: bootcamps
         });
-    } catch {
+    } catch(err) {
         res.status(400).json({ 
             success: false
         });
     }
 }
 
-exports.getBootcamp = async (req, res) => {
+exports.getBootcamp = async (req, res, next) => {
     try{
         const bootcamp = await Bootcamp.findById(req.params.id);
         if (!bootcamp)
-            return res.status(400).json({ 
-                success: false
-            });
+            return next(new ErrorResponse(`Bootcamp not faund with id of ${req.params.id}`, 404));
         res.status(200).json({ 
             success: true,
             data: bootcamp
         });
-    } catch {
-        res.status(400).json({ 
-            success: false
-        });
+    } catch (err){
+        next(new ErrorResponse(`Bootcamp not faund with id of ${req.params.id}`, 404));
     }
 }
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db')
+const errorHandler = require('./middleware/error');
 
 
 // Load env vars
@@ -26,6 +27,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
     res.send("Hello there!");
