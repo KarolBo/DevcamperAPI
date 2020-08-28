aee21b8ff22527cb47679f113d0e383b9d0a3be1
Karol Borkowski <karol.borkowski@b-rayz.ch>
2020-08-28 21:07:30 +0200

    mongo

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -1,19 +1,86 @@
-exports.getBootcamps = (req, res) => {
-     res.status(200).json({ success: true, msg: "show all bootcamps"});
+const Bootcamp = require('../models/Bootcamp');
+
+exports.getBootcamps = async (req, res) => {
+    try{
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({ 
+            success: true,
+            count: bootcamps.length,
+            data: bootcamps
+        });
+    } catch {
+        res.status(400).json({ 
+            success: false
+        });
+    }
 }
 
-exports.getBootcamp = (req, res) => {
-    res.status(200).json({ success: true, msg: `show bootcamp ${req.params.id}`});
+exports.getBootcamp = async (req, res) => {
+    try{
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        if (!bootcamp)
+            return res.status(400).json({ 
+                success: false
+            });
+        res.status(200).json({ 
+            success: true,
+            data: bootcamp
+        });
+    } catch {
+        res.status(400).json({ 
+            success: false
+        });
+    }
 }
 
-exports.createBootcamp = (req, res) => {
-    res.status(200).json({ success: true, msg: "create new bootcamp"});
+exports.createBootcamp = async (req, res) => {
+    try {
+        const bootcamp = await Bootcamp.create(req.body);
+        res.status(201).json({ 
+            success: true, 
+            data: bootcamp
+        });
+    } catch {
+        res.status(400).json({ success: false });
+    }
+    
 }
 
-exports.updateBootcamp = (req, res) => {
-    res.status(200).json({ success: true, msg: `update bootcamp ${req.params.id}`});
+exports.updateBootcamp = async (req, res) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!bootcamp)
+            return res.status(400).json({ 
+                success: false
+            });
+
+        res.status(201).json({ 
+            success: true, 
+            data: bootcamp
+        });
+    } catch {
+        res.status(400).json({ success: false });
+    }
 }
 
-exports.deleteBootcamp = (req, res) => {
-    res.status(200).json({ success: true, msg: `delete bootcamp ${req.params.id}`});
+exports.deleteBootcamp = async (req, res) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+        if (!bootcamp)
+            return res.status(400).json({ 
+                success: false
+            });
+            
+        res.status(201).json({ 
+            success: true, 
+            data: bootcamp
+        });
+    } catch {
+        res.status(400).json({ success: false });
+    }
 }
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db')
+
+
+// Load env vars
+dotenv.config({ path: './config/config.env'});
+
+connectDB();
 
 const logger = require('./middleware/logger');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
-// Load env vars
-dotenv.config({ path: './config/config.env'});
-
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
     app.use(logger);
@@ -18,11 +25,19 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Mount routers
-app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/bootcamps', bootcamps);
 
 app.get('/', (req, res) => {
     res.send("Hello there!");
 });
 
-app.listen(process.env.PORT || 5000, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`));
+const server = app.listen(
+    process.env.PORT || 5000, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
+    );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+});
