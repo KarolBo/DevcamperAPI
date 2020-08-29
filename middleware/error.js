107fca73ca72196483fa2bacc7621633c8caaf8d
Karol Borkowski <karol.borkowski@b-rayz.ch>
2020-08-29 11:00:12 +0200

    async error handling

--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,30 @@
-const errorHandler = (err, req, res, next) => {
-    console.log(err.stack);
+const ErrorResponse = require('../utils/errorResponse');
 
-    res.status(err.statusCode || 500).json({
+const errorHandler = (err, req, res, next) => {
+    console.log(err);
+    let error;
+    // Mongoose bad ObjectId
+    if (err.name === 'CastError') {
+        const message = `Resource not faund with id of ${err.value}`;
+        error = new ErrorResponse(message, 404);
+    } 
+    // Mongoose duplicate key
+    else if (err.code === 11000) {
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+    //Mongoose validation error
+    else if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map( val => val.message);
+        error = new ErrorResponse(message, 400);
+    }
+    else {
+        error = err;
+    }
+
+    res.status(error.statusCode || 500).json({
         success: false,
-        error: err.message || 'Server Error'
+        error: error.message
     });
 };
 
