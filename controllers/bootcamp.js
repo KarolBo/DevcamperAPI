243d19d72f1c33f32f8972bf1b45305b1a402588
Karol Borkowski <karol.borkowski@b-rayz.ch>
2020-09-06 13:00:02 +0200

    Propagation & cascade delete

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -22,7 +22,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
         // Finding resources 
         let query = JSON.parse(queryStr);
-        query = Bootcamp.find(query);
+        query = Bootcamp.find(query).populate('courses');
 
          // Select Fields
          if (req.query.select) {
@@ -108,11 +108,14 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 });
 
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
 
     if (!bootcamp)
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
         
+    bootcamp.remove();
+
     res.status(201).json({ 
         success: true, 
         data: bootcamp
